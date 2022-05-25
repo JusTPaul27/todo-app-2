398cc7742252ef13dfddce5a0564ab4783fd2885
Paolo Foppiano <103486420+JusTPaul27@users.noreply.github.com>
2022-05-25 13:25:28 +0200

    push delle 13.25

--- todo.js
+++ todo.js
@@ -34,3 +34,25 @@ console.log(params);
 // }
 
 // getTodoFromSessionStorage();
+
+function createEditTemplate(){
+    const editTemplate = `
+    <h2>nome</h2>
+   <input type="text" name="todo-name" id="todo-name">
+   <div>
+       <h2>Tags</h2>
+       <span class="tag">casa</span>
+       <span class="tag">hobby</span>
+       <span class="tag">spesa</span>
+       <span class="tag">fumetti</span>
+       <span class="tag">lavoro</span>
+   </div>
+       <div>
+           <h2>priorità</h2>
+           <span class="tag">bassa</span>
+           <span class="tag">media</span>
+           <span class="tag">alta</span>
+           <span class="tag">molto alta</span>
+       </div>`
+
+}
